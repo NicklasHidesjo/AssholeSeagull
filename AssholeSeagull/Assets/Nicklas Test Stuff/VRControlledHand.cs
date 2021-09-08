1aab732d65581c8370b770e80a59893e5c49a50d
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2021-09-08 19:32:44 +0200

    Update VRControlledHand.cs

--- AssholeSeagull/Assets/Nicklas Test Stuff/VRControlledHand.cs
+++ AssholeSeagull/Assets/Nicklas Test Stuff/VRControlledHand.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.XR.Interaction.Toolkit;
 
 public class VRControlledHand : MonoBehaviour
 {
