310f84937450c69ba7a06bc0f80907cdf1b9e077
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2021-09-09 10:48:25 +0200

    remade project

---
+++ AssholeSeagull/Assets/Nicklas Test Stuff/VRControlledHand.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+//using UnityEngine.InputSystem;
+
+public class VRControlledHand : MonoBehaviour
+{
+
+	public SteamVR_Input_Sources handType;
+	public SteamVR_Action_Boolean grabAction;
+
+	private void Update()
+	{
+		bool isPressed = grabAction.GetState(handType);
+
+		if(isPressed)
+		{
+			Debug.Log("Pressing" + handType);
+		}
+
+	}
+
+		/*	[SerializeField] float forceMultiplier;
+
+			bool itemInHand;
+			Vector3 throwForce;
+
+			[SerializeField] InteractableItem interactable;
+
+			[SerializeField] InputActionReference grabInteraction;
+
+			private void Start()
+			{
+				grabInteraction.action.performed += Gripped;
+			}
+
+			private void Gripped(InputAction.CallbackContext context)
+			{
+				Debug.Log("I am retarded");
+				if (itemInHand)
+				{
+					interactable.DropItem(throwForce * forceMultiplier);
+					interactable = null;
+					itemInHand = false;
+				}
+				else if (interactable != null)
+				{
+					interactable.PickUp(gameObject);
+					itemInHand = true;
+				}
+			}
+
+
+			private void OnTriggerEnter(Collider other)
+			{
+				if (itemInHand)
+				{ return; }
+				if (other.gameObject.GetComponent<InteractableItem>() != null)
+				{
+					interactable = other.gameObject.GetComponent<InteractableItem>();
+				}
+			}
+
+			private void OnTriggerExit(Collider other)
+			{
+				if (itemInHand)
+				{ return; }
+				if (other.gameObject.GetComponent<InteractableItem>() != null)
+				{
+					interactable = null;
+				}
+			}*/
+	}
+
