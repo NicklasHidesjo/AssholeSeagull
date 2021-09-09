3b9424f43d800865b7c172e89d3bb9a637b1e8f0
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2021-09-09 09:05:51 +0200

    Update HPTest.cs

--- AssholeSeagull/Assets/Nicklas Test Stuff/HPTest.cs
+++ AssholeSeagull/Assets/Nicklas Test Stuff/HPTest.cs
@@ -26,12 +26,12 @@ public class HPTest : MonoBehaviour
             Debug.Log("Found more than one left hand!");
         }
 
-        var rightHandedDevices = new List<UnityEngine.XR.InputDevice>();
-        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.LeftHand, leftHandDevices);
+        var rightHandDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.RightHand, rightHandDevices);
 
-        if (rightHandedDevices.Count == 1)
+        if (rightHandDevices.Count == 1)
         {
-            UnityEngine.XR.InputDevice rightHand = rightHandedDevices[0];
+            UnityEngine.XR.InputDevice rightHand = rightHandDevices[0];
             Debug.Log(string.Format("Device name '{0}' with role '{1}'", rightHand.name, rightHand.characteristics.ToString()));
         }
         else if (leftHandDevices.Count > 1)
