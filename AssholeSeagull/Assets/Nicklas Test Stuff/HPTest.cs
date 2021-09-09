2549e97333fdb38a39b40dd24a72dbcc94a7f88b
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2021-09-09 09:00:16 +0200

    Update HPTest.cs

--- AssholeSeagull/Assets/Nicklas Test Stuff/HPTest.cs
+++ AssholeSeagull/Assets/Nicklas Test Stuff/HPTest.cs
@@ -15,7 +15,7 @@ public class HPTest : MonoBehaviour
     {
         var inputDevices = new List<UnityEngine.XR.InputDevice>();
         UnityEngine.XR.InputDevices.GetDevices(inputDevices);
-
+        Debug.Log(inputDevices.Count);
         foreach (var device in inputDevices)
         {
             Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.characteristics.ToString()));
