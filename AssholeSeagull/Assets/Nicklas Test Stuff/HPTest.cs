ecf334276411527603569c6c0a6bd29b395fc046
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2021-09-09 09:04:25 +0200

    Update HPTest.cs

--- AssholeSeagull/Assets/Nicklas Test Stuff/HPTest.cs
+++ AssholeSeagull/Assets/Nicklas Test Stuff/HPTest.cs
@@ -7,18 +7,36 @@ public class HPTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var inputDevices = new List<UnityEngine.XR.InputDevice>();
-        UnityEngine.XR.InputDevices.GetDevices(inputDevices);
-        Debug.Log(inputDevices.Count);
-        foreach (var device in inputDevices)
-        {
-            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.characteristics.ToString()));
-        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        var leftHandDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.LeftHand, leftHandDevices);
 
+        if (leftHandDevices.Count == 1)
+        {
+            UnityEngine.XR.InputDevice leftHand = leftHandDevices[0];
+            Debug.Log(string.Format("Device name '{0}' with role '{1}'", leftHand.name, leftHand.characteristics.ToString()));
+        }
+        else if (leftHandDevices.Count > 1)
+        {
+            Debug.Log("Found more than one left hand!");
+        }
+
+        var rightHandedDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.LeftHand, leftHandDevices);
+
+        if (rightHandedDevices.Count == 1)
+        {
+            UnityEngine.XR.InputDevice rightHand = rightHandedDevices[0];
+            Debug.Log(string.Format("Device name '{0}' with role '{1}'", rightHand.name, rightHand.characteristics.ToString()));
+        }
+        else if (leftHandDevices.Count > 1)
+        {
+            Debug.Log("Found more than one left hand!");
+        }
     }
 }
