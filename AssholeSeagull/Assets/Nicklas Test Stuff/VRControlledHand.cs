7583c8a10ec589463157ba28a82826aa18390c15
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2021-09-09 11:01:01 +0200

    can you pick up now?

--- AssholeSeagull/Assets/Nicklas Test Stuff/VRControlledHand.cs
+++ AssholeSeagull/Assets/Nicklas Test Stuff/VRControlledHand.cs
@@ -6,9 +6,15 @@ using Valve.VR;
 
 public class VRControlledHand : MonoBehaviour
 {
+	[SerializeField] float forceMultiplier;
 
-	public SteamVR_Input_Sources handType;
-	public SteamVR_Action_Boolean grabAction;
+	bool itemInHand;
+	Vector3 throwForce;
+
+	[SerializeField] InteractableItem interactable;
+
+	[SerializeField] SteamVR_Input_Sources handType;
+	[SerializeField] SteamVR_Action_Boolean grabAction;
 
 	private void Update()
 	{
@@ -16,60 +22,40 @@ public class VRControlledHand : MonoBehaviour
 
 		if(isPressed)
 		{
-			Debug.Log("Pressing" + handType);
+			if (itemInHand)
+			{
+				interactable.DropItem(throwForce * forceMultiplier);
+				interactable = null;
+				itemInHand = false;
+			}
+			else if (interactable != null)
+			{
+				interactable.PickUp(gameObject);
+				itemInHand = true;
+			}
 		}
 
 	}
 
-		/*	[SerializeField] float forceMultiplier;
 
-			bool itemInHand;
-			Vector3 throwForce;
-
-			[SerializeField] InteractableItem interactable;
-
-			[SerializeField] InputActionReference grabInteraction;
-
-			private void Start()
-			{
-				grabInteraction.action.performed += Gripped;
-			}
-
-			private void Gripped(InputAction.CallbackContext context)
-			{
-				Debug.Log("I am retarded");
-				if (itemInHand)
-				{
-					interactable.DropItem(throwForce * forceMultiplier);
-					interactable = null;
-					itemInHand = false;
-				}
-				else if (interactable != null)
-				{
-					interactable.PickUp(gameObject);
-					itemInHand = true;
-				}
-			}
-
-
-			private void OnTriggerEnter(Collider other)
-			{
-				if (itemInHand)
-				{ return; }
-				if (other.gameObject.GetComponent<InteractableItem>() != null)
-				{
-					interactable = other.gameObject.GetComponent<InteractableItem>();
-				}
-			}
-
-			private void OnTriggerExit(Collider other)
-			{
-				if (itemInHand)
-				{ return; }
-				if (other.gameObject.GetComponent<InteractableItem>() != null)
-				{
-					interactable = null;
-				}
-			}*/
+	private void OnTriggerEnter(Collider other)
+	{
+		if (itemInHand)
+		{ return; }
+		if (other.gameObject.GetComponent<InteractableItem>() != null)
+		{
+			interactable = other.gameObject.GetComponent<InteractableItem>();
+		}
 	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (itemInHand)
+		{ return; }
+		if (other.gameObject.GetComponent<InteractableItem>() != null)
+		{
+			interactable = null;
+		}
+	}
+}
 
