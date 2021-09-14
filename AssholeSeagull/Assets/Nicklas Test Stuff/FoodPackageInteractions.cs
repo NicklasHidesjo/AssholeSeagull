cff442f12550a258f9dd410aaede5ecb3acf038d
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2021-09-14 10:39:16 +0200

    first try spawning food in hand

---
+++ AssholeSeagull/Assets/Nicklas Test Stuff/FoodPackageInteractions.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Valve.VR.InteractionSystem
+{
+	//-------------------------------------------------------------------------
+	[RequireComponent(typeof(Interactable))]
+	public class FoodPackageInteractions : MonoBehaviour
+	{
+		[SerializeField] FoodPackage package;
+
+		public UnityEvent onHandHoverBegin;
+		public UnityEvent onHandHoverEnd;
+		private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.SnapOnAttach) & (~Hand.AttachmentFlags.DetachOthers) & (~Hand.AttachmentFlags.VelocityMovement);
+
+		//-------------------------------------------------
+		private void OnHandHoverBegin()
+		{
+			onHandHoverBegin.Invoke();
+		}
+
+
+		//-------------------------------------------------
+		private void OnHandHoverEnd()
+		{
+			onHandHoverEnd.Invoke();
+		}
+
+		//-------------------------------------------------
+		private void OnAttachedToHand(Hand hand)
+		{
+			GameObject gameObject = package.SpawnFoodItem();
+			GrabTypes startingGrabType = hand.GetGrabStarting();
+			hand.AttachObject(gameObject, startingGrabType, attachmentFlags);
+		}
+	}
+}
+
--- AssholeSeagull/Assets/Scripts/FoodPackage.cs
+++ AssholeSeagull/Assets/Scripts/FoodPackage.cs
@@ -4,53 +4,42 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
-namespace Valve.VR.InteractionSystem.Sample
+public class FoodPackage : MonoBehaviour
 {
-    [RequireComponent(typeof(Interactable))]
-    public class FoodPackage : MonoBehaviour
+    [SerializeField] private FoodItem foodItem;
+    [SerializeField] private Transform parent;
+    [SerializeField] private string foodName;
+    [SerializeField] private GameObject poop;
+    private bool shitOnPackage;
+    private int spoiledFoods = 0;
+
+    public bool ShitOnPackage
     {
-        [SerializeField] private FoodItem foodItem;
-        [SerializeField] private Transform parent;
-        [SerializeField] private string foodName;
-        [SerializeField] private GameObject poop;
-        private bool shitOnPackage;
-        private int spoiledFoods = 0;
-
-        public bool ShitOnPackage
+        get { return shitOnPackage; }
+        set
         {
-            get { return shitOnPackage; }
-            set
+            shitOnPackage = value;
+            poop.SetActive(value);
+            
+            if (value)
             {
-                shitOnPackage = value;
-                poop.SetActive(value);
-
-                if (value)
-                {
-                    spoiledFoods = 1;
-                }
+                spoiledFoods = 1;
             }
         }
+    }
 
-        private void Start()
-        {
-            ShitOnPackage = true;
-            SpawnFoodItem(transform);
-            SpawnFoodItem(parent);
-        }
+    public GameObject SpawnFoodItem()
+    {
+        Debug.Log("Item spawned");
+        FoodItem newFoodItem = Instantiate(foodItem);
+        newFoodItem.name = foodName;
+        newFoodItem.PoopOnFood = shitOnPackage;
 
-        public void SpawnFoodItem(Transform foodParent)
-        {
-            Debug.Log("Item spawned");
-            FoodItem newFoodItem = Instantiate(foodItem, foodParent, true);
-            newFoodItem.name = foodName;
-            newFoodItem.GetComponent<Rigidbody>().isKinematic = true; //Delete after testing
+        spoiledFoods--;
+        spoiledFoods = (int) Mathf.Clamp(spoiledFoods, 0, Mathf.Infinity);
+        shitOnPackage = spoiledFoods > 0;
+        poop.SetActive(shitOnPackage);
 
-            newFoodItem.PoopOnFood = shitOnPackage;
-
-            spoiledFoods--;
-            spoiledFoods = (int)Mathf.Clamp(spoiledFoods, 0, Mathf.Infinity);
-            shitOnPackage = spoiledFoods > 0;
-            poop.SetActive(shitOnPackage);
-        }
+        return newFoodItem.gameObject;
     }
 }
