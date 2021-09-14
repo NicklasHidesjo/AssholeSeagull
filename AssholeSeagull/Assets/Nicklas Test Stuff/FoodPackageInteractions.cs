be3a782bab9a27d5f7dab41ce5f5a2dbabc5bd67
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2021-09-14 10:50:57 +0200

    Update FoodPackageInteractions.cs

--- AssholeSeagull/Assets/Nicklas Test Stuff/FoodPackageInteractions.cs
+++ AssholeSeagull/Assets/Nicklas Test Stuff/FoodPackageInteractions.cs
@@ -13,6 +13,8 @@ namespace Valve.VR.InteractionSystem
 
 		public UnityEvent onHandHoverBegin;
 		public UnityEvent onHandHoverEnd;
+		public UnityEvent onAttachedToHand;
+		public UnityEvent onDetachedFromHand;
 		private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.SnapOnAttach) & (~Hand.AttachmentFlags.DetachOthers) & (~Hand.AttachmentFlags.VelocityMovement);
 
 		//-------------------------------------------------
@@ -32,11 +34,17 @@ namespace Valve.VR.InteractionSystem
 		//-------------------------------------------------
 		private void OnAttachedToHand(Hand hand)
 		{
+			onAttachedToHand.Invoke();
 			Debug.Log("Hand touched");
 
 			GameObject gameObject = package.SpawnFoodItem();
 			GrabTypes startingGrabType = hand.GetGrabStarting();
 			hand.AttachObject(gameObject, startingGrabType, attachmentFlags);
+		}
+
+		private void OnDetachedFromHand(Hand hand)
+		{
+			onDetachedFromHand.Invoke();
 		}
 	}
 }
