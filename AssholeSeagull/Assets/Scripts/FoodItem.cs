810deb895faa0f039460c669d112d8b9f85a47ce
TosaLipkiss <tovahl2002@skola.goteborg.se>
2021-09-17 11:18:15 +0100

    Food layers and raycast

--- AssholeSeagull/Assets/Scripts/FoodItem.cs
+++ AssholeSeagull/Assets/Scripts/FoodItem.cs
@@ -14,6 +14,8 @@ public class FoodItem : MonoBehaviour
 
     [SerializeField] private float timer;
     [SerializeField] private bool isSpoiled;
+    [SerializeField] Material spoiledMaterial;
+
     private bool inHand;
 
     private bool onSandwich;
@@ -21,7 +23,27 @@ public class FoodItem : MonoBehaviour
     private bool inPackage;
     private bool buttered;
     private bool onPlate;
-       
+
+    public enum FoodTypes
+    {
+        None,
+        Bread,
+        Ham,
+        Cheese,
+    }
+
+    [SerializeField] LayerMask foodLayer;
+    [SerializeField] FoodTypes foodType;
+    [SerializeField] FoodTypes foodAbove;
+    [SerializeField] FoodTypes foodBelow;
+
+    [SerializeField] float rayDistance;
+
+    public FoodTypes FoodType
+    {
+        get { return foodType; }
+    }
+
     public bool InHand
     {
         get { return inHand; }
@@ -37,25 +59,26 @@ public class FoodItem : MonoBehaviour
         set { poopOnFood = value; }
     }
     public bool InPackage
-	{
+    {
         get { return inPackage; }
         set { inPackage = value; }
-	}
+    }
     public bool Buttered
-	{
-		get { return buttered; }
+    {
+        get { return buttered; }
         set { buttered = value; }
-	}
+    }
 
     private void Start()
     {
         body = GetComponent<Rigidbody>();
         gameManager = FindObjectOfType<GameManager>();
-
     }
 
     private void Update()
     {
+        RaycastFoodLayer();
+
         if (onSandwich || inHand || inPackage || buttered)
         {
             return;
@@ -69,15 +92,26 @@ public class FoodItem : MonoBehaviour
         timer += Time.deltaTime;
         isSpoiled = timer > spoilTime;
 
-        if(timer > selfDestructTime && !onPlate)
-		{
+        bool alreadySpoiled = false;
+
+        //Varför gör den inget
+        if (isSpoiled && alreadySpoiled == false)
+        {
+            alreadySpoiled = true;
+            Debug.Log("SPPPPOIIILED");
+            Material myMaterial = GetComponent<MeshRenderer>().material;
+            myMaterial = spoiledMaterial;
+        }
+
+        if (timer > selfDestructTime && !onPlate)
+        {
             Destroy(gameObject);
-		}
+        }
     }
 
     private void OnTriggerEnter(Collider collider)
     {
-        if(collider.tag == "Plate")
+        if (collider.tag == "Plate")
         {
             gameManager.score++;
             onPlate = true;
@@ -92,6 +126,53 @@ public class FoodItem : MonoBehaviour
             gameManager.score--;
             onPlate = false;
             Debug.Log("Score: " + gameManager.score);
+        }
+    }
+
+    void RaycastFoodLayer()
+    {
+        RaycastHit hit;
+
+        Vector3 northSide = new Vector3(0, rayDistance, 0);
+        Vector3 southSide = new Vector3(0, -rayDistance, 0);
+
+        Debug.DrawRay(transform.position, northSide, Color.blue);
+        Debug.DrawRay(transform.position, southSide, Color.red);
+
+        if(Physics.Linecast(transform.position, northSide, out hit, foodLayer))
+        {
+            FoodItem food = hit.collider.gameObject.GetComponent<FoodItem>();
+
+            if (food == null)
+            {
+                foodAbove = FoodTypes.None;
+                return;
+            }
+
+            Debug.Log("Above raycast hit: " + food);
+            foodAbove = food.foodType;
+        }
+        else
+        {
+            foodAbove = FoodTypes.None;
+        }
+
+        if (Physics.Linecast(transform.position, southSide, out hit, foodLayer))
+        {
+            FoodItem food = hit.collider.gameObject.GetComponent<FoodItem>();
+
+            if (food == null)
+            {
+                foodBelow = FoodTypes.None;
+                return;
+            }
+
+            Debug.Log("Below raycast hit: " + food);
+            foodBelow = food.foodType;
+        }
+        else
+        {
+            foodAbove = FoodTypes.None;
         }
     }
 }
