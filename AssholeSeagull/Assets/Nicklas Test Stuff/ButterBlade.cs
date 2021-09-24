cf516ca60c6d541d4f473ace5db9986502ecc3ca
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2021-09-24 16:03:36 +0200

    fixed butter on knife and plate stuff

--- AssholeSeagull/Assets/Nicklas Test Stuff/ButterBlade.cs
+++ AssholeSeagull/Assets/Nicklas Test Stuff/ButterBlade.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class ButterBlade : MonoBehaviour
 {
 	private bool butterOnBlade;
+	[SerializeField] GameObject butterOnKnife;
 
 	public bool ButterOnBlade
 	{
-		get { return butterOnBlade; } set { butterOnBlade = value; }
+		get { return butterOnBlade; } 
+		set 
+		{ 
+			butterOnBlade = value;
+			butterOnKnife.SetActive(value);
+		}
 	}
 }
