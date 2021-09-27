1921ed53ed502fa66fdacb2b6c9b58ee74154c7d
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2021-09-27 10:31:16 +0200

    rough implementation of scaring

--- AssholeSeagull/Assets/WaveSeagull.cs
+++ AssholeSeagull/Assets/WaveSeagull.cs
@@ -4,18 +4,66 @@ using UnityEngine;
 
 public class WaveSeagull : MonoBehaviour
 {
+    [SerializeField] float velocityMultiplier;
+    [SerializeField] float scareVelocityThreshold;
+    
     [SerializeField] Transform headTransform;
     [SerializeField] Transform rightHandTransform;
     [SerializeField] Transform leftHandTransform;
+
+    Vector3 oldLeftPosition;
+    Vector3 oldRightPosition;
+
     void Start()
     {
-        
+        oldLeftPosition = leftHandTransform.position;
+        oldRightPosition = rightHandTransform.position;
     }
 
     void Update()
     {
         Debug.Log("Head y position is: " + headTransform.position.y);
-        Debug.Log("Right-hand y position is: " + rightHandTransform.position.y);
-        Debug.Log("Left-hand y position is: " + leftHandTransform.position.y);
+
+        Vector3 headPos = headTransform.position;
+
+        Vector3 rightHandPos = rightHandTransform.position;
+        Vector3 leftHandPos = leftHandTransform.position;
+
+        Debug.Log("Left-hand y position is: " + leftHandPos.y);
+        Debug.Log("Right-hand y position is: " + rightHandPos.y);
+
+
+        if (rightHandPos.y > headPos.y)
+        {
+            float velocity = GetSpeed(rightHandPos, oldRightPosition);
+
+            Debug.Log("Right hand velocity is: " + velocity);
+
+            if(velocity > scareVelocityThreshold)
+            {
+                Debug.Log("Scaring seagull using right hand");
+            }
+        }
+        if (leftHandPos.y > headPos.y)
+        {
+            float velocity = GetSpeed(leftHandPos, oldLeftPosition);
+
+            Debug.Log("Left hand velocity is: " + velocity);
+
+            if (velocity > scareVelocityThreshold)
+            {
+                Debug.Log("Scaring seagull using left hand");
+            }
+        }
+
+        oldLeftPosition = leftHandPos;
+        oldRightPosition = rightHandPos;
+    }
+
+    private float GetSpeed(Vector3 currentPos, Vector3 oldPosition)
+    {
+        float distanceTraveled = Vector3.Distance(oldPosition, currentPos);
+        distanceTraveled = Mathf.Abs(distanceTraveled);
+        return distanceTraveled * velocityMultiplier;
     }
 }
