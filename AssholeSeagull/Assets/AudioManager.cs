89924af593b3905f67e06045043a997099a6fae3
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2021-09-28 20:06:00 +0200

    Update AudioManager.cs
    fixed sound bug.

--- AssholeSeagull/Assets/AudioManager.cs
+++ AssholeSeagull/Assets/AudioManager.cs
@@ -29,7 +29,7 @@ public class AudioManager : MonoBehaviour
         rightHand.PointerClick += PointerClick;
         leftHand.PointerClick += PointerClick;
 
-        buttonPlayer = FindObjectOfType<AudioSource>();
+        buttonPlayer = GetComponent<AudioSource>();
 
         LoadVolume();
     }
